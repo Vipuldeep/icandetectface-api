43394dcd72b5012d55dba6de4de9f3604d1367f8
Vipul <vipuldeep95@gmail.com>
2021-07-01 12:43:57 +1000

    back end

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ const database = {
 }
 
 app.get('/', (req, res) =>{
-    res.send(database.users);
-    // res.send('this is working');
+    // res.send(database.users);
+    res.send('this is working');
 })
 
 app.post('/signin', (req, res) => {
